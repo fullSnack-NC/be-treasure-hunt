dabfed5cc14a48f9f4b99b18ed4fc745f1c77583
nicolalockwood <nicolahelenlockwood@gamil.com>
2022-05-04 16:49:20 +0100

    error handling controller added

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,15 +1,21 @@
-const db = require("../db/connection");
-const seed = require("../db/seeds/seed");
-const testData = require("../db/data/test-data");
-const app = require("../app");
-const request = require("supertest");
+const db = require('../db/connection');
+const seed = require('../db/seeds/seed');
+const testData = require('../db/data/test-data');
+const app = require('../app');
+const request = require('supertest');
 
 afterAll(() => {
-  db.end();
+	db.end();
 });
 
 beforeEach(() => seed(testData));
 
-test("should first", () => {
-  console.log("testing");
+describe('GET /api/:town_id/parks');
+test('200 blah blah blah ', () => {
+	console.log('testing');
+});
+
+describe('ERROR HANDLING /api/:town_id/parks');
+test('404 blah blah blah ', () => {
+	console.log('testing');
 });
