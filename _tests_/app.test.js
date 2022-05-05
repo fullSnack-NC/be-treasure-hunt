e199dc8e387f29c9a8b65c4871fc63f0eff14ebe
iblis <chris.strafford@gmail.com>
2022-05-05 13:11:32 +0100

    GET parks by park_id completed with error handling

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,57 +1,59 @@
-const db = require('../db/connection');
-const seed = require('../db/seeds/seed');
-const testData = require('../db/data/test-data');
-const app = require('../app');
-const request = require('supertest');
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+const app = require("../app");
+const request = require("supertest");
 
 afterAll(() => {
-	db.end();
+  db.end();
 });
 
 beforeEach(() => seed(testData));
 
-describe('GET api/parks', () => {
-	test('status:200, responds with all parks', async () => {
-		const res = await request(app).get('/api/parks').expect(200);
+describe("GET api/parks", () => {
+  test("status:200, responds with all parks", async () => {
+    const res = await request(app).get("/api/parks").expect(200);
 
-		expect(res.body.parks).toBeInstanceOf(Object);
-		res.body.parks.forEach((park) => {
-			expect(park).toMatchObject({
-				park_id: expect.any(Number),
-				town_id: expect.any(Number),
-				park_name: expect.any(String),
-				parks_lat: expect.any(String),
-				parks_long: expect.any(String),
-				amenities: expect.any(String),
-			});
-		});
-	});
+    expect(res.body.parks).toBeInstanceOf(Object);
+    res.body.parks.forEach((park) => {
+      expect(park).toMatchObject({
+        park_id: expect.any(Number),
+        town_id: expect.any(Number),
+        park_name: expect.any(String),
+        parks_lat: expect.any(String),
+        parks_long: expect.any(String),
+        amenities: expect.any(String),
+      });
+    });
+  });
 });
 
-describe('ERROR HANDLING api/parks', () => {
-	test('status:400, responds with bad request message when wrong api is passes ', async () => {
-		const res = await request(app).get('/api/10').expect(404);
-		expect(res.body.msg).toEqual('Path not found');
-	});
+describe("ERROR HANDLING api/parks", () => {
+  test("status:400, responds with bad request message when wrong api is passes ", async () => {
+    const res = await request(app).get("/api/10").expect(404);
+    expect(res.body.msg).toEqual("Path not found");
+  });
 });
 
-describe('GET api/parks/:park_id', () => {
-	test('status:200, responds with 1 park by park_id', async () => {
-		const res = await request(app).get('/api/parks/1').expect(200);
-		expect(res.body.parks).toBeInstanceOf(Object);
-		expect(res.body.parks).toMatchObject({
-			park_id: 1,
-			town_id: 4,
-			park_name: 'Roundhay Park',
-			parks_lat: '53.83767',
-			parks_long: '-1.495378',
-			amenities: {"{
-				"wildlife": true,
-				"lake": true,
-				"toilets": true,
-				"food": true,
-				"accessible": false,
-			}"
-		});
-	});
+describe("GET api/parks/:park_id", () => {
+  test("status:200, responds with 1 park by park_id", async () => {
+    const res = await request(app).get("/api/parks/2").expect(200);
+    expect(res.body.parks).toBeInstanceOf(Object);
+    expect(res.body.parks).toEqual({
+      park_id: 2,
+      town_id: 2,
+      park_name: "Rothwell Park",
+      parks_lat: "53.76074",
+      parks_long: "1.472666",
+      amenities:
+        '{"food": true, "lake": true, "toilets": true, "wildlife": true, "accessible": true}',
+    });
+  });
+});
+
+describe("ERROR HANDLING api/parks/:park_id", () => {
+  test("status:400, responds with bad request message when wrong api is passed", async () => {
+    const res = await request(app).get("/api/parks/VillaPark").expect(400);
+    expect(res.body.msg).toEqual("Bad request");
+  });
 });
--- models/parks.model.js
+++ models/parks.model.js
@@ -1,18 +1,18 @@
-const { query } = require('../db/connection');
-const db = require('../db/connection');
-const format = require('pg-format');
-const parks = require('../db/data/test-data/parks');
+const { query } = require("../db/connection");
+const db = require("../db/connection");
+const format = require("pg-format");
+const parks = require("../db/data/test-data/parks");
 
 exports.selectParks = async () => {
-	const parks = await db.query(`SELECT * FROM parks`);
-	return parks.rows;
+  const parks = await db.query(`SELECT * FROM parks`);
+  return parks.rows;
 };
 
 exports.selectParksById = async (park_id) => {
-	const parkById = await db.query(`SELECT * FROM parks WHERE park_id =$1`, [
-		park_id,
-	]);
+  const parkById = await db.query(`SELECT * FROM parks WHERE park_id =$1`, [
+    park_id,
+  ]);
 
-	console.log(parkById.rows[0]);
-	return parkById.rows[0];
+  console.log(parkById.rows);
+  return parkById.rows[0];
 };
