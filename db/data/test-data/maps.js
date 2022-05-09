935e5f3712beeba087964ad64c8cf2b833d2a524
Gareth Everson <gareth@garetheverson.com>
2022-05-09 18:06:53 +0100

    Adding Maps images

--- db/data/test-data/maps.js
+++ db/data/test-data/maps.js
@@ -1,30 +1,56 @@
 module.exports = [
   {
     park_id: 1,
-    map_name: "Roundhay Loop",
+    map_name: 'Roundhay Loop',
     length: 1.6,
     est_comp_time: 30,
     age_min: 5,
+    image:
+      'https://drive.google.com/file/d/12jGrgNGiY64l_iPRZx-WGvrJXbsk9V-y/view?usp=sharing',
   },
   {
     park_id: 2,
-    map_name: "Rothwell Loop",
+    map_name: 'Rothwell Loop',
     length: 4.2,
     est_comp_time: 51,
     age_min: 6,
+    image:
+      'https://drive.google.com/file/d/1-GteYo0yfVhVt-YvduFHoddGJ8IamI91/view?usp=sharing',
   },
   {
     park_id: 2,
-    map_name: "Rothwell Extreme",
+    map_name: 'Rothwell Extreme',
     length: 1.2,
     est_comp_time: 30,
     age_min: 3,
+    image:
+      'https://drive.google.com/file/d/17PqswZ7cL2kTnMviA9VEiNFn1rzxjdyj/view?usp=sharing',
   },
   {
     park_id: 3,
-    map_name: "Bird watching",
+    map_name: 'Bird watching',
     length: 5,
     est_comp_time: 75,
     age_min: 7,
+    image:
+      'https://drive.google.com/file/d/14LPTJbxi_GwTK15bA0BVYmtLHqweGNx-/view?usp=sharing',
+  },
+  {
+    park_id: 1,
+    map_name: 'Roundhay Explorer',
+    length: 4.2,
+    est_comp_time: 50,
+    age_min: 7,
+    image:
+      'https://drive.google.com/file/d/1iljSCa2tfczRlHzPPFL4zGy0JArduVh6/view?usp=sharing',
+  },
+  {
+    park_id: 1,
+    map_name: 'Waterloo Lake',
+    length: 2.7,
+    est_comp_time: 44,
+    age_min: 5,
+    image:
+      'https://drive.google.com/file/d/1cF6fyKNKLNu56bCUaeh36dRGJs9-nhRB/view?usp=sharing',
   },
 ];
