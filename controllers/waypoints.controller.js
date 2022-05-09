e0869008683643e1fafec7c4e5a54a1be3aba5f1
almcw <98993527+almcw@users.noreply.github.com>
2022-05-09 11:06:41 +0100

    add endpoint for /api/waypoints/park_id

--- controllers/waypoints.controller.js
+++ controllers/waypoints.controller.js
@@ -1,8 +1,8 @@
 const { selectWaypointByMapId } = require("../models/waypoints.model.js");
 
-exports.getWaypointByMapId = (req, res, next) => {
+exports.getWaypointsByMapId = (req, res, next) => {
   const { map_id } = req.params;
-  selectWaypointByMapId(req.body, map_id)
+  selectWaypointByMapId(map_id)
     .then((waypoint) => {
       res.status(200).send({ waypoint });
     })
