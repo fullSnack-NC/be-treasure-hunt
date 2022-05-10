40e3a87fe6d49e8dd4f441a76c682751e10d46ed
iblis <chris.strafford@gmail.com>
2022-05-10 11:42:58 +0100

    merged

--- db/data/test-data/parks.js
+++ db/data/test-data/parks.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     town_id: 4,
-    park_name: 'Roundhay Park',
+    park_name: "Roundhay Park",
     parks_lat: 53.83767,
     parks_long: -1.495378,
     amenities: {
@@ -12,11 +12,11 @@ module.exports = [
       accessible: false,
     },
     image:
-      'https://drive.google.com/file/d/1U5nVKMLKETcOW7SNQVilxrozjGmeF1nP/view?usp=sharing',
+      "https://drive.google.com/file/d/1U5nVKMLKETcOW7SNQVilxrozjGmeF1nP/view?usp=sharing",
   },
   {
     town_id: 2,
-    park_name: 'Rothwell Park',
+    park_name: "Rothwell Park",
     parks_lat: 53.76074,
     parks_long: 1.472666,
     amenities: {
@@ -27,11 +27,11 @@ module.exports = [
       accessible: true,
     },
     image:
-      'https://drive.google.com/file/d/1R-0fPCTykXge0CH-xnExRn9pKsXq0m5Y/view?usp=sharing',
+      "https://drive.google.com/file/d/1R-0fPCTykXge0CH-xnExRn9pKsXq0m5Y/view?usp=sharing",
   },
   {
     town_id: 3,
-    park_name: 'RSPB St. Aidans',
+    park_name: "RSPB St. Aidans",
     parks_lat: 53.7531,
     parks_long: 1.3964,
     amenities: {
@@ -42,6 +42,36 @@ module.exports = [
       accessible: true,
     },
     image:
-      'https://drive.google.com/file/d/1nird2zqAE8FKZsw02YHtXYDh-OETVb3O/view?usp=sharing',
+      "https://drive.google.com/file/d/1nird2zqAE8FKZsw02YHtXYDh-OETVb3O/view?usp=sharing",
+  },
+  {
+    town_id: 1,
+    park_name: "Thorns Park",
+    parks_lat: 53.6729621,
+    parks_long: -1.5152618,
+    amenities: {
+      wildlife: true,
+      lake: false,
+      toilets: false,
+      food: false,
+      accessible: true,
+    },
+    image:
+      "https://drive.google.com/file/d/1nird2zqAE8FKZsw02YHtXYDh-OETVb3O/view?usp=sharing",
+  },
+  {
+    town_id: 4,
+    park_name: "Golden Acre Park",
+    parks_lat: 53.871287,
+    parks_long: -1.4858004,
+    amenities: {
+      wildlife: true,
+      lake: true,
+      toilets: false,
+      food: false,
+      accessible: false,
+    },
+    image:
+      "https://drive.google.com/file/d/1nird2zqAE8FKZsw02YHtXYDh-OETVb3O/view?usp=sharing",
   },
 ];
